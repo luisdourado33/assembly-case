af1dd7327482c8a7eb1e0564f42256b129442aea
Luís Dourado <52614953+luisdourado33@users.noreply.github.com>
2021-03-29 09:26:12 -0300

    feat: Multiplicação de pontos flutuantes implementada

--- mips1.asm
+++
@@ -1,6 +0,0 @@
-.data
-	msg: .asciiz "Hello World!"
-.text
-	li $v0, 4 # Print a String or Char
-	la $a0, msg
-	syscall
--- multiply.asm
+++ multiply.asm
@@ -1,29 +1,38 @@
 .data
-	precoCaixa: .asciiz "Digite o preco da caixa: "
-	qtde: .asciiz "Quantidade de caixas: "
+	msgPreco: .asciiz "Digite o valor da caixa: "
+	msgQtde: .asciiz "Quantidade de caixas: "
 	
-	valorFinal: .asciiz "Total a pagar: \n"
-	baixo: .asciiz "\nValor (baixo aproximado): R$ "
-	alto: .asciiz "\nValor (alto aproximado): R$"
-
+	preco: .float 0.0
+	qtde: .float 0.0
+	valorFinal: .float 0.0
 .text
-	.globl main
-
-main:
-	li $v0, 4
-	la $a0, precoCaixa
+	li $v0, 4		# Carrega o código que lê String
+	la $a0, msgPreco	# Imprime msgPreco
 	syscall
 	
-	li $v0, 6
+	li $v0, 6		# Carrega o codigo que lê Float
 	syscall
 	
-	move $a0, $f0
+	s.s $f0, preco		# Aloca o valor lido na variavel preco
+	
+	li $v0, 4		# Carrega o codigo que lê String
+	la $a0, msgQtde		# Imprime msgQtde
 	syscall
 	
-	li $v0, 2
-	la $a0, precoCaixa
+	li $v0, 6		# Carrega o código que lê Float
 	syscall
-
-End_Prog:
-	li $v0, 10
+	
+	s.s $f0, qtde		# Aloca o valor lido na variavel qtde
+	la $a0, preco		# Carrega o valor de preco em a0
+	l.s $f1, 0($a0)		# Define f1 com o valor de a0 (preco)
+	
+	la $a0, qtde		# Aloca o valor de qtde em a0
+	l.s $f2, 0($a0)		# Define f2 com o valor de a0 (qtde)
+	
+	mul.s $f1, $f1, $f2	# Realiza a multiplicacao de pontos flutuantes $f1 = $f1 + $f2
+	s.s $f1, valorFinal	# Aloca o valor da multiplicacao em f1
+	
+	li $v0, 2		# Carrega o codigo que le float
+	l.s $f12, valorFinal	# Imprime float (valorFinal)
 	syscall
+	
