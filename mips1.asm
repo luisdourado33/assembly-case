ff516ad4d0282d3e483a81c0c3fee003e959542b
Luís Dourado <52614953+luisdourado33@users.noreply.github.com>
2021-03-28 23:05:56 -0300

    news: Hello World

--- mips1.asm
+++ mips1.asm
@@ -1,5 +1,8 @@
 .data
-
-
-
+	# Área para dados na memória principal
+	msg: .asciiz "Hello World!" # Mensagem a ser exibida ao usuário
 .text
+	# Área para instruções do programa
+	li $v0, 4 # Instrução para impressão de String
+	la $a0, msg # Indica o endereço em que está a mensagem
+	syscall # Faça! Imprima!
